cfadaf4558157fbd5ec1c53322663236a604098a
Jheisson Villafuerte Sullon <jvillafuerte@deltasac.com.pe>
2023-10-18 13:39:53 -0500

    Agregar archivos de proyecto.

---
+++ SOL_JHEISSON_VILLAFUERTE/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SOL_JHEISSON_VILLAFUERTE
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ SOL_JHEISSON_VILLAFUERTE/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SOL_JHEISSON_VILLAFUERTE.MvcApplication" Language="C#" %>
